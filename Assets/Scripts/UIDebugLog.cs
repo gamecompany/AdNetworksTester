b9533508539af3916156100b324826c6b707142e
Axel Jimenez <limbo.axel@gmail.com>
2016-05-31 11:24:26 -0700

    UI to play ads and display messages

---
+++ Assets/Scripts/UIDebugLog.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDebugLog : MonoBehaviour
+{
+    //
+    // UIDebugLog
+    // structs & classes
+
+    // • • • • • • • • • • • • • • • • • • • • //
+
+    //
+    // V a r i a b l e s
+    //
+
+    [SerializeField]
+    private Text console;
+
+	// • • • • • • • • • • • • • • • • • • • • //
+
+	//
+	// P r o p e r t i e s
+	//
+	
+	// • • • • • • • • • • • • • • • • • • • • //
+
+	//
+	// U n i t y
+	//
+
+	// • • • • • • • • • • • • • • • • • • • • //
+
+	//
+	// U s e r
+	//
+
+    public void Display(string text)
+    {
+        if (!console) return;
+
+        console.text = text;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AdNetworksTester
 A test application for different ad networks
+
+This repository has code for using different ad networks. Most of the code comes from the ad networks themselves, and is under difference licenses.
+This code is not open-source
