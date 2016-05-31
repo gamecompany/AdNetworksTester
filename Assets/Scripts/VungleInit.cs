bba3168f64b3a76ec74a676b9e050dd78693105c
macasillas <miguel@miguelcasillas.com>
2016-05-31 17:39:12 -0500

    Integrating vungle

---
+++ Assets/Scripts/VungleInit.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VungleInit : MonoBehaviour
+{
+    public string AndroidID;
+    public string iOSID;
+    public string WindowsID;
+
+    public UIDebugLog DebugLog;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        Vungle.init(AndroidID, iOSID, WindowsID);
+
+        Vungle.adPlayableEvent += (isAdAvailable) => {
+            if (isAdAvailable)
+            {
+                DebugLog.Display("An ad is ready to show!");
+            }
+            else {
+                DebugLog.Display("No ad is available at this moment.");
+            }
+        };
+    }
+
+    public void WatchAd()
+    {
+        Dictionary<string, object> options = new Dictionary<string, object>();
+        options["incentivized"] = true;
+        Vungle.playAdWithOptions(options);
+    }
+}
